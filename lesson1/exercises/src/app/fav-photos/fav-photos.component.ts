fc18ad6b2c96e4b02e40d4f4b5cb143a38eb08ab
Kaleb Lucas <klucas765@gmail.com>
2019-12-11 19:16:29 -0600

    Added functionality to first angular exercise.

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.thesprucecrafts.com/thmb/pL3jukiNt9d2kf4kd6Jtjtq9kZk=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-90339990-586b54d73df78ce2c33d2deb.jpg';
+  image3 = 'https://www.papajohns.com/static-assets/a/images/papa-johns-logo-201905211749.png';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', '2001: A Space Oddysey', 'Indiana Jones and the Temple of Doom'];
 
    constructor() { }
 
